d81998313b623763f633f049d205868b2043351e
vikash <vikash.tomar.india@gmail.com>
2023-09-17 00:25:47 +0530

    update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
         return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
      }
 
-     function capitalChar()
+     function upperChar()
      {
         return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
      }
@@ -17,4 +17,30 @@
         const symbol = "!@#$%^&*()[]"
         return symbol[Math.floor(Math.random() * symbol.length )]
      }
-    console.log(getRandomSymbol())
+    // console.log(getRandomSymbol())
+    const lengthEl = document.getElementById("length")
+    const uppercaseEl = document.getElementById("uppercase")
+    const lowercaseEl = document.getElementById("lowercase")
+    const numberEl = document.getElementById("number")
+    const symbolEl = document.getElementById("symbols")
+    const generateEl = document.getElementById("generate")
+    const clipboardEl = document.getElementById("clipboard")
+
+
+    const randomFunc = {
+        lower: lowerChar,
+        upper: upperChar,
+        number: randomNum,
+        symbol: getRandomSymbol
+    };
+
+
+    generateEl.addEventListener("click",()=>{
+        const length = Number(lengthEl.value);
+        const hasLower = lowercaseEl.checked;
+        const hasUpper = uppercaseEl.checked;
+        const hasnumber = numberEl.checked;
+        const hassymbol = symbolEl.checked;
+        console.log( length, hasLower , hasUpper, hasnumber , hassymbol)
+
+    })
