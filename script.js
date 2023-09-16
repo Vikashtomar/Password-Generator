a1466bd9f5c5f5b6d0e3348b7b89ee654f7c8447
vikash <vikash.tomar.india@gmail.com>
2023-09-16 23:57:44 +0530

    update

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
  // Lets start making password generator
 
-    function lowerabc(){
-        return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+     function lowerChar()
+     {
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+     }
 
-    }
-    console.log(lowerabc())
+     function capitalChar()
+     {
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+     }
+     function randomNum()
+     {
+        return String.fromCharCode(Math.floor(Math.random() * 10 ) + 48)
+     }
+     function getRandomSymbol(){
+        const symbol = "!@#$%^&*()[]"
+        return symbol[Math.floor(Math.random() * symbol.length )]
+     }
+    console.log(getRandomSymbol())
